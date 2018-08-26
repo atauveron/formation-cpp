d353f1579d2c85341bfeabf8fcf4205b55f7dc48
Aimery Tauveron <atauveron@via.ecp.fr>
2018-08-26 12:41:35 +0200

    Added slides on move semantics
    Added slides on move semantics in cpp-moderne.tex
    Added slide on default methods in cpp-poo.tex
    
    Added example code for move semantics in blob.cpp

---
+++ code/blob.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <cstring>
+
+class blob {
+private:
+  size_t m_size;
+  char *m_data;
+  
+public:
+
+  blob (): m_size(0), m_data(nullptr) {}
+  
+  blob (const char* d, size_t s): m_size(s) {
+    if (m_size) {
+      m_data = new char[m_size];
+      memcpy(m_data, d, m_size);
+    } else {
+      m_data = nullptr;
+    }
+  }
+  
+  ~blob () {
+    delete[] m_data;
+  }
+
+  blob (const blob& other): m_size(other.m_size) {
+    if (m_size) {
+      m_data = new char[m_size];
+      memcpy(m_data, other.m_data, m_size);
+    } else {
+      m_data = nullptr;
+    }
+  }
+
+  // blob (blob&& other) {
+  //   m_size = other.m_size;
+  //   m_data = other.m_data;
+  //   other.m_size = 0;
+  //   other.m_data = nullptr;
+  // }
+
+  friend void swap (blob& first, blob& second) {
+    using std::swap;
+    //
+    swap(first.m_size, second.m_size);
+    swap(first.m_data, second.m_data);
+  }
+
+  blob (blob&& other): blob() {
+    swap(*this, other);
+  }
+
+  blob& operator= (blob other) {
+    swap(*this, other);
+    return *this;
+  }
+};
+
+int main ()
+{
+  std::cout << "Hello World!"  << std::endl;
+  return 0;
+}
