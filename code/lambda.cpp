e5ad06a3fd8229845100d146ae52cb3337cd6f9b
Aimery Tauveron <atauveron@via.ecp.fr>
2018-08-27 15:38:12 +0200

    Added slides on lambdas
    Updated README to include GitHub link to beamer theme
    
    Added slides on use of move semantics when managing ressources in
    cpp-moderne.tex
    Added slides on lambdas in cpp-moderne.tex
    
    Added example code for lambdas in code/lambdas.cpp

---
+++ code/lambda.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+namespace {
+  void f (int i) {
+    std::cout << i << ' ';
+  }
+}
+
+int example () {
+  std::vector<int> vec;
+  vec.push_back(10);
+  vec.push_back(20);
+  vec.push_back(30);
+  vec.push_back(40);
+  vec.push_back(50);
+  
+  // Using a functor
+  for_each(vec.begin(), vec.end(), f);
+  std::cout << '\n';
+  // Using a lambda
+  for_each(vec.begin(), vec.end(), [](int i) {
+      std::cout << i << ' ';
+    } );
+  std::cout << '\n';
+  
+  return 0;
+}
+
+int example_double () {
+  std::vector<double> position;
+  position.push_back(.1);
+  position.push_back(.2);
+  position.push_back(.3);
+  position.push_back(.4);
+  position.push_back(.5);
+
+  double scale = 5;
+
+  for_each(position.begin(), position.end(), [&scale](double x) {
+      std::cout << scale*x << ' ';
+    } );
+  std::cout << '\n';
+
+  return 0;  
+}
+
+int main () {
+  example();
+  example_double();
+  
+  return 0;
+}
